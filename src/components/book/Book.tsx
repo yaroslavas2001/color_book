2dd6f7565c9477053fcc18f07334a42e338b80ae
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2023-05-02 14:28:09 +0300

    pages

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
 import style from "./Book.module.css"
+import chroma from "chroma-js"
+import Page from "./../page/Page"
 function Book() {
-    return (
-        <div className={style.book}>
+    let size = 10
+    const mode = "lab"
+    // left-top  to left-bottom
+    let startColors: Array<string> = ["white", "yellow", "red", "#E04E7B"]
+    let endColors: Array<string> = ["violet", "green", "black", "blue"]
 
+    let scale = (color1: string, color2: string): Array<string> => {
+        return chroma.scale([color1, color2])
+            .mode(mode).colors(size)
+    }
+    let leftTop = scale(startColors[0], endColors[0])
+    let rightTop = scale(startColors[1], endColors[1])
+    let rightBottom = scale(startColors[2], endColors[2])
+    let leftBottom = scale(startColors[3], endColors[3])
+
+    let book = []
+    for (let i = 0; i < size; i++) {
+        book.push([leftTop[i], rightTop[i], rightBottom[i], leftBottom[i]])
+    }
+    let bookElement = book.map((el,index) => <Page key={index} colors={el}></Page>)
+
+    return (
+        <div className={style.wrapper}>
+            <input type="color" />
+            {bookElement}
+            {/* <Page colors={startColors} /> */}
         </div>
+
     );
 }
 
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import { FC } from "react"
 import style from "./Page.module.css"
-function Book() {
-    return (
-        <div className={style.page}>
+import chroma from "chroma-js"
+type propsPage = {
+    colors: Array<string>
+}
+let Page: FC<propsPage> = ({ colors }) => {
 
+    let size = 100
+    // lab
+    // lrgb
+    const mode = "lab"
+    let scale = (color1: string, color2: string): Array<string> => {
+        return chroma.scale([color1, color2])
+            .mode(mode).colors(size)
+    }
+    let left = scale(colors[0], colors[2])
+    let right = scale(colors[1], colors[3])
+   
+    let ar = []
+   
+    for (let i = 0; i < size; i++) {
+        const leftColor = left[i];
+        const rightColor = right[i];
+        let grad = chroma.scale([leftColor, rightColor])
+            .mode(mode).colors(size)
+        ar.push(grad)
+    }
+   
+   let page = ar.map((el, index) => <div key={index} className={style.row}>
+        {el.map((color, index) => <span key={index} style={{ background: color }} className={style.block}></span>)}
+    </div>
+    )
+
+    return (
+        <div className={style.wrapper}>
+            {page}
         </div>
+
     );
 }
 
-export default Book;
+export default Page;
