708190fe579bb0fa86d28fa7d038ec83fc9e9b94
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2023-05-04 15:51:04 +0300

    page numbers

--- src/components/book/Book.tsx
+++ src/components/book/Book.tsx
@@ -4,19 +4,19 @@ import Page from "./../page/Page"
 import { FC, useCallback } from "react";
 import HTMLFlipBook from 'react-pageflip';
 import { Color } from "../../App";
-type propsBook={
-    colors:Array<Color>
+type propsBook = {
+    colors: Array<Color>
 }
-let Book:FC<propsBook>=({colors}) =>{
+let Book: FC<propsBook> = ({ colors }) => {
     let size = 10
-    
+
     const mode = "lab"
     // left-top  to left-bottom
     let scale = (color1: string, color2: string): Array<string> => {
         return chroma.scale([color1, color2])
             .mode(mode).colors(size)
     }
-    
+
     let leftTop = scale(colors[0].color, colors[4].color)
     let rightTop = scale(colors[1].color, colors[5].color)
     let rightBottom = scale(colors[2].color, colors[6].color)
@@ -26,14 +26,15 @@ let Book:FC<propsBook>=({colors}) =>{
     for (let i = 0; i < size; i++) {
         book.push([leftTop[i], rightTop[i], rightBottom[i], leftBottom[i]])
     }
-    let bookElement = book.map((el, index) => <Page key={index} colors={el}></Page>)
+
+    let bookElement = book.map((el, index) => <Page isViceVersa={index % 2 !== 0} key={index} pageNumber={index + 1} colors={el} />)
 
     const onFlip = useCallback((e: any) => {
         console.log('Current page: ' + e.data);
     }, []);
+
     return (
         <div className={style.wrapper}>
-            {/* <input type="color" /> */}
             <HTMLFlipBook
                 onFlip={onFlip}
                 width={300}
@@ -51,7 +52,7 @@ let Book:FC<propsBook>=({colors}) =>{
                 startPage={0}
                 startZIndex={0}
                 autoSize={true}
-                maxShadowOpacity={0.9}
+                maxShadowOpacity={0.5}
                 showCover={false}
                 mobileScrollSupport={true}
                 clickEventForward={true}
@@ -63,7 +64,6 @@ let Book:FC<propsBook>=({colors}) =>{
                 {bookElement}
             </HTMLFlipBook>
         </div>
-
     );
 }
 
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -3,8 +3,10 @@ import style from "./Page.module.css"
 import chroma from "chroma-js"
 type propsPage = {
     colors: Array<string>
+    pageNumber: number
+    isViceVersa: boolean
 }
-let Page: FC<propsPage> = ({ colors }) => {
+let Page: FC<propsPage> = ({ colors, pageNumber, isViceVersa }) => {
 
     let size = 100
     // lab
@@ -20,29 +22,30 @@ let Page: FC<propsPage> = ({ colors }) => {
     let ar = []
 
     for (let i = 0; i < size; i++) {
-        const leftColor = left[i];
-        const rightColor = right[i];
+        const leftColor = isViceVersa ? left[i] : right[i];
+        const rightColor = isViceVersa ? right[i] : left[i];
         let grad = chroma.scale([leftColor, rightColor])
             .mode(mode).colors(size)
         ar.push(grad)
     }
 
     let page = ar.map((el, index) => <div key={index} className={style.row}>
-        {el.map((color, index) => 
-        <span key={index} style={{ background: color }} className={style.block}></span>)}
+        {el.map((color, index) =>
+            <span key={index} style={{ background: color }} className={style.block}></span>)}
     </div>
     )
 
     return (
         <div className={style.wrapper}>
             {page}
+            <div className={isViceVersa?style.viceVersa:""}>{pageNumber}</div>
         </div>
     );
 }
 const PageRef = React.forwardRef((props: any, ref: any) => {
     return (
-        <div className={style.bigPage} ref={ref}>
-            <Page colors={props.colors} />
+        <div ref={ref}>
+            <Page isViceVersa={props.isViceVersa} colors={props.colors} pageNumber={props.pageNumber} />
         </div>
     );
 });
